6677c276be47fbe0f1e9f094eca4b0868812a831
AnandBhattacharya98 <anandb9198@gmail.com>
2022-02-19 22:59:20 -0700

    Question 1d
    The test fails for decryption

--- TestJunit.java
+++ TestJunit.java
@@ -3,10 +3,17 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestJunit {
-   @Test
+   /**@Test
    public void testEncrypt() {
       cipher test_cipher = new cipher();
       String str = test_cipher.encrypt("helloWORLD", "aBcD").toString();
       assertEquals("zwddgOGJDV",str);
+   }*/
+
+   @Test
+   public void testDecrypt() {
+      cipher test_cipher = new cipher();
+      String str = test_cipher.decrypt("zwddgOGJDV", "aBcD").toString();
+      assertEquals("helloWORLD",str);
    }
 }
