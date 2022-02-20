66517c806ec0c00f8eb59fecceeb98cd902965ac
AnandBhattacharya98 <anandb9198@gmail.com>
2022-02-19 22:53:11 -0700

    Question 1c
    After updating encryption function, the test now passes.

--- cipher.java
+++ cipher.java
@@ -8,8 +8,16 @@ class cipher
 
 		for (int i=0; i<text.length(); i++)
 		{
-			char ch = text.charAt(i);
-			result.append(ch);
+			if (Character.isUpperCase(text.charAt(i)))
+			{
+				char ch = (char)(((int)text.charAt(i) + s - 65) % 26 + 65);
+				result.append(ch);
+			}
+			else
+			{
+				char ch = (char)(((int)text.charAt(i) + s - 97) % 26 + 97);
+				result.append(ch);
+			}
 		}
 		return result;
 	}
